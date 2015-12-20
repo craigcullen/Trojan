24f22251287fbe5edb483a29e2766c2f478c2440
craigcullen <craig340@gmail.com>
2015-12-20 15:02:53 -0700

    Changes in file_id

--- modules/file_id.py
+++ modules/file_id.py
@@ -1,14 +1,8 @@
 import os
 
-file_id = []
-
 def run(**args):
-    print "[*] In dirlister module."
-    files = os.listdir(".")
-    for file in files:
-        file_id.append(file)
-       
-    file_1 = file_id[0]
-    rfile = file_1.read()
-        
-    return rfile
+    print "[*] In file_id module."
+    with open ('dirlister.py', 'rb') as file:
+        file.read()
+    
+    return file
