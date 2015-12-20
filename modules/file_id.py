46f5a5f14bff3e6a0b7104a4c78ba0f4a8c621f3
craigcullen <craig340@gmail.com>
2015-12-20 08:51:27 -0700

    New file_id

--- modules/file_id.py
+++ modules/file_id.py
@@ -5,7 +5,8 @@ file_id = []
 def run(**args):
     print "[*] In dirlister module."
     files = os.listdir(".")
-    for file in files:
-        file_id.append(os.fileno(file))
-    
-    return file_id
+    for file[0] in files:
+        file_id = file[0]
+        file_1 = file_id.read()
+        
+        return file_1
