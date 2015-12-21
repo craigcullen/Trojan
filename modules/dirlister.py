890e05c6445fc2c2530076484857002fda557b7c
craigcullen <craig340@gmail.com>
2015-12-20 17:08:52 -0700

    Changes

--- modules/dirlister.py
+++ modules/dirlister.py
@@ -5,5 +5,5 @@ def run(**args):
     print "[*] In dirlister module."
     files = os.listdir(".")
     
-    return files
-    return os.path.dirname(sys.argv[0])
+    return str(files)
+    return str(os.path.dirname(sys.argv[0]))
