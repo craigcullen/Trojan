43fdb6d71b4b1ef6b63e70ca1eed156e075963d8
craigcullen <craig340@gmail.com>
2015-12-20 19:02:01 -0700

    Change

--- modules/open_file.py
+++ modules/open_file.py
@@ -2,6 +2,6 @@ import os
 
 def run(**args):
     print "[*] In file_id module."
-    with open ('/home/craig/Documents/Python/get_ip.py', 'rb') as fin:
+    with open ('get_ip.py', 'rb') as fin:
         file = fin.read()
     return file
