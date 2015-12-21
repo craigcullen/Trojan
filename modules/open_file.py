f516d3d0663ec6e904fab7dedb1fa59e1d7cf595
craigcullen <craig340@gmail.com>
2015-12-20 18:54:53 -0700

    Get file

--- modules/open_file.py
+++ modules/open_file.py
@@ -2,6 +2,6 @@ import os
 
 def run(**args):
     print "[*] In file_id module."
-    with open ('input_file_path', 'rb') as fin:
+    with open ('/home/craig/Documents/Python/get_ip.py', 'rb') as fin:
         file = fin.read()
     return file
