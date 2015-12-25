9dc84321555e412a7e64c6d69ecc83445e4a97fe
craigcullen <craig340@gmail.com>
2015-12-25 10:02:34 -0700

    Changed open_jpeg.py

--- modules/open_jpeg.py
+++ modules/open_jpeg.py
@@ -3,6 +3,6 @@ from PIL import Image
 
 def run(**args):
     print "[*] In open_jpeg module."
-    kate = Image.open("/home/craig/Pictures/Kate-Beckinsale.jpg")
-    kate = kate.load()
-    return kate
+    with open("/home/craig/Pictures/Kate-Beckinsale.jpg","rb") as kate_image:
+        kate_image = base64.b64encode(kate_image.read()) 
+    return kate_image
