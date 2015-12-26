f0b31e4284aaaf5fd054e9f57c8b8723bf80ca45
craigcullen <craig340@gmail.com>
2015-12-25 17:47:42 -0700

    Changed file

--- modules/open_jpeg.py
+++ modules/open_jpeg.py
@@ -2,6 +2,5 @@ from PIL import Image
 
 def run(**args):
     print "[*] In open_jpeg module."
-    with open("/home/craig/Pictures/Kate-Beckinsale.jpg","rb") as kate_image:
-        kate_image = kate_image.read() 
-    return kate_image
+    with open("/home/craig/Pictures/Kate-Beckinsale.jpg","rb") as kate_image: 
+        return kate_image.read()
