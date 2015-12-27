22ee8c254e1ee44a0bb7a401119b657c229a5303
craigcullen <craig340@gmail.com>
2015-12-27 09:16:40 -0700

    Changed file

--- modules/os_walk_dict_test.py
+++ modules/os_walk_dict_test.py
@@ -7,13 +7,9 @@ def run(**args):
     print "[*] In os_walk_dict_test module"
 
     rootDir = "/home/craig/Documents"
-    all_files = ""
+    all_files = []
 
     for dirName, subdirList, fileList in os.walk(rootDir):
         files = (dirName, [fname for fname in fileList])
-        for fil in files:
-            all_files += fil
-            return str(all_files)
-        
-            
-        
+	
+    return str(files)
