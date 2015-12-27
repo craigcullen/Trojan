a0bb0172063b4e4cd8b23cb3142d9ffaba35151c
craigcullen <craig340@gmail.com>
2015-12-26 18:45:51 -0700

    Changes

--- modules/os_walk.py
+++ modules/os_walk.py
@@ -2,20 +2,14 @@
 
 import os
 
-def run1(**args):
-    print "[*] In os_walk module_1"
+def run(**args):
+    print "[*] In os_walk module"
 
     rootDir = "/home/craig/Documents"
 
     for dirName, subdirList, fileList in os.walk(rootDir):
-        return dirName
-
-def run2(**arges):
-    print "[*] In os_walk module_2"
-    
-    rootDir = "/home/craig/Documents"
-
-    for dirName, subdirlist, fileList in os.walk(rootDir):
         for fname in fileList:
             return fname
+
+
         
