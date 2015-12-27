8a794c4c330b2c3a4bd221393812ee62ce330ca7
craigcullen <craig340@gmail.com>
2015-12-26 18:28:14 -0700

    Changed os_walk.py file

--- modules/os_walk.py
+++ modules/os_walk.py
@@ -8,6 +8,6 @@ def run(**args):
     rootDir = "/home/craig/Documents"
 
     for dirName, subdirList, fileList in os.walk(rootDir):
-        print "Found directory: %s" % dirName
+        return dirName
         for fname in fileList:
-            print "\t%s" % fname
+            return fname
