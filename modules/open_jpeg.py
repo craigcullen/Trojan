b6615a9ae1bc92eb507f7d1a8c67b01f3e2a9619
craigcullen <craig340@gmail.com>
2015-12-30 17:43:31 -0700

    Changed files

--- modules/open_jpeg.py
+++ modules/open_jpeg.py
@@ -4,5 +4,5 @@ def run(**args):
     print "[*] In open_jpeg module."
     kate = Image.open("/home/craig/Pictures/Kate-Beckinsale.jpg")
     size = 120, 120
-   return(kate.thumbnail(size))
+    return(kate.thumbnail(size))
 
