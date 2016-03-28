a5625aaadbf4b0aa029eecf99e55fa880b4de0bb
craigcullen <craig340@gmail.com>
2016-03-27 19:59:52 -0600

    change file

--- modules/login_name_jpeg_test.py
+++ modules/login_name_jpeg_test.py
@@ -1,12 +1,11 @@
 import os
-import re
 
 from PIL import Image
 
 def run(**args):
     print "[*] In login_name_jpeg module."
     name = str(os.getlogin())
-    pic = Image.open(str(re.compile("\/home\/" + name + "\/Pictures\/^.*\.jpg")))
+    pic = Image.open("/home/" + name + "/Pictures/Kate-Beckinsale.jpg")
     size = 120, 120
     pic.thumbnail(size)
     pic.save("/home/" + name + "/Pictures/KB" + ".thumbnail", "JPEG")
