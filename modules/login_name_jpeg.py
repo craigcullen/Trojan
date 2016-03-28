e0c9e4e55e3917346578ed1e70a656e59f7c9179
craigcullen <craig340@gmail.com>
2016-03-27 18:41:46 -0600

    changed file

--- modules/login_name_jpeg.py
+++ modules/login_name_jpeg.py
@@ -3,12 +3,10 @@ import re
 
 from PIL import Image
 
-def run1(**args):
+def run(**args):
     print "[*] In login_name module."
     name = str(os.getLogin())
     return str(os.getlogin())
-
-def run2(**args):
     print "[*] In open_jpeg module."
     pic = Image.open("/home/" + name + "/Pictures/" + re.match("^.*") + ".jpg")
     size = 120, 120
